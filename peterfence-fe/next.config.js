772349abb7cef555b084697ec9fe99696639b11d
hristohpavlov <hristohpavlov@gmail.com>
2023-02-20 00:28:09 +0200

    dasad

--- peterfence-fe/next.config.js
+++ peterfence-fe/next.config.js
@@ -2,12 +2,7 @@
 const path = require('path');
 
 const nextConfig = {
-  distDir: 'peterfence-fe/out',
   target: 'serverless',
-  env: {
-    MONGO_URI: "mongodb+srv://admin:admin@cluster0.9u6f5kl.mongodb.net/nextAuthDB?retryWrites=true&w=majority",
-    SERVER_URL: "https://peterfence-backend.herokuapp.com",
-  },
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
@@ -15,7 +10,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig;
-
-const withOptimizedImages = require('next-optimized-images')
-
-module.exports = withOptimizedImages({})

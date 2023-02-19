c1a274e07079203958e0b1112d71b79f46d3b4b0
hristohpavlov <hristohpavlov@gmail.com>
2023-02-19 20:01:39 +0200

    sdaasdad

--- peterfence-fe/next.config.js
+++ peterfence-fe/next.config.js
@@ -2,6 +2,7 @@
 const path = require('path');
 
 const nextConfig = {
+  target: 'serverless',
   env: {
     MONGO_URI: "mongodb+srv://admin:admin@cluster0.9u6f5kl.mongodb.net/nextAuthDB?retryWrites=true&w=majority",
     SERVER_URL: "https://peterfence-backend.herokuapp.com",

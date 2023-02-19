063a24a372f9b01cf26f48cae0bf4eb49492182b
hristohpavlov <hristohpavlov@gmail.com>
2023-02-19 19:26:41 +0200

    Rewrite frontend part

--- peterfence-fe/next.config.js
+++ peterfence-fe/next.config.js
@@ -7,6 +7,7 @@ const nextConfig = {
     SERVER_URL: "https://peterfence-backend.herokuapp.com",
   },
   reactStrictMode: true,
+  target: "serverless",
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },

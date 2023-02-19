68b49dae75c9c2209cec2cb5f26b5cd241511be3
hristohpavlov <hristohpavlov@gmail.com>
2023-02-19 19:53:20 +0200

    lmml;kj

--- peterfence-fe/next.config.js
+++ peterfence-fe/next.config.js
@@ -7,7 +7,6 @@ const nextConfig = {
     SERVER_URL: "https://peterfence-backend.herokuapp.com",
   },
   reactStrictMode: true,
-  target: "serverless",
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },

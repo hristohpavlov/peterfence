98fcfa06f29c89fd3902f467ced0d84608e86f33
hristohpavlov <hristohpavlov@gmail.com>
2023-02-23 03:58:23 +0200

    lkljlkj

--- peterfence-fe/lib/mongodb.js
+++ peterfence-fe/lib/mongodb.js
@@ -10,6 +10,7 @@ async function connect(){
     }
     const db = await mongoose.connect(process.env.MONGODB_URI);
     connection.isConnected = db.connections[0].readyState;
+    mongoose.disconnect();
 }
 
 export default connect;

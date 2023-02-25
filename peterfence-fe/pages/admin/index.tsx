5810a7c8caa700e82dcb36b2740b903121e884f7
hristohpavlov <hristohpavlov@gmail.com>
2023-02-25 19:03:04 +0200

    asdadad

--- peterfence-fe/pages/admin/index.tsx
+++ peterfence-fe/pages/admin/index.tsx
@@ -11,8 +11,7 @@ export default function Admin(){
                     <h2 className="p-3">Admin Panel</h2>
                 </div>
                 <div className="card-body">
-                    <form name="contact-form" action="/api/login" method="POST" netlify-honeypot="bot-field" data-netlify="true">
-                    <input type='hidden' name='form-name' value='contact-form' />
+                    <form name="contact-form" action="/api/login" method="POST">
                     <div className="mb-4">
                         <label htmlFor="email" className="form-label">Email</label>
                         <input type="email" name="email" className="form-control" id="email" />
